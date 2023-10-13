2309fcc9e660b46b22058b1e3c764c1f71f3ad93
Vadim <vademcheg@gmail.com>
2023-10-13 19:02:45 +0400

    dev

--- middlewares/logsMiddleware.js
+++ middlewares/logsMiddleware.js
@@ -5,18 +5,30 @@ export async function logsMiddleware({ request, payload }) {
 
   const data = {};
   data.version = 1;
-  data.payload = payload;
-  data.accessData = request?.accessData;
+  data.accessData = request?.accessData || '?';
+  data.method = request?.method;
+  data.pathname = request?.url?.pathname;
+  data.payloadBase64 = Buffer.from(payload).toString('base64');
 
   const type = `Request ${request?.method || ''} ${request?.url?.pathname || ''}`
 
-  return await sql`
-    INSERT INTO public."logs" (
-      "data",
-      "type"
-    ) VALUES (
-      '${JSON.stringify(data)}',
-      '${type}'
-    )
-  `;
+  console.log ({data:JSON.stringify(data), type})
+
+  let result;
+  try {
+    result = await sql`
+      INSERT INTO public."logs" (
+        "data",
+        "type"
+      ) VALUES (
+        '${JSON.stringify(data)}',
+        '${type}'
+      )
+    `;
+  } catch (error) {
+    console.log({error})
+  }
+  
+
+  return result;
 };
